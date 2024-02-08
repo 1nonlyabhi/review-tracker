c62f9ae9c35839597627ae02c4061567a8714823
1nonlyabhi <gabhishek0407@gmail.com>
2024-02-08 23:37:21 +0530

    mvn fmt:format

--- review-engine/src/main/java/com/zughead/reviewengine/ReviewEngineApp.java
+++ review-engine/src/main/java/com/zughead/reviewengine/ReviewEngineApp.java
@@ -2,13 +2,11 @@ package com.zughead.reviewengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.core.GrantedAuthorityDefaults;
 
 @SpringBootApplication
 public class ReviewEngineApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReviewEngineApp.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ReviewEngineApp.class, args);
+  }
 }
--- review-engine/src/main/java/com/zughead/reviewengine/security/user/User.java
+++ review-engine/src/main/java/com/zughead/reviewengine/security/user/User.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +17,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
-
 @Data
 @Entity
 @Builder
@@ -25,57 +24,57 @@ import java.util.List;
 @AllArgsConstructor
 @Table(name = "_user")
 public class User implements UserDetails {
-    @Id
-    @GeneratedValue
-    @Column(name = "id", nullable = false)
-    private Long id;
+  @Id
+  @GeneratedValue
+  @Column(name = "id", nullable = false)
+  private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    @Column(nullable = false)
-    private String firstname;
+  @Column(nullable = false)
+  private String firstname;
 
-    private String lastname;
+  private String lastname;
 
-    @Column(nullable = false)
-    private String password;
+  @Column(nullable = false)
+  private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+  @Enumerated(EnumType.STRING)
+  private Role role;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
+  @Override
+  public String getPassword() {
+    return password;
+  }
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
+  @Override
+  public String getUsername() {
+    return email;
+  }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
