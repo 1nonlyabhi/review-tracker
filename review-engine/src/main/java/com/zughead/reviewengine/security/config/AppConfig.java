d35734a0ac63f80d2c2dfcbc79bd5f0efcbe3937
1nonlyabhi <gabhishek0407@gmail.com>
2024-02-10 17:39:42 +0530

    add security n auth config

--- review-engine/src/main/java/com/zughead/reviewengine/security/config/AppConfig.java
+++ review-engine/src/main/java/com/zughead/reviewengine/security/config/AppConfig.java
@@ -4,8 +4,14 @@ import com.zughead.reviewengine.security.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -18,5 +24,23 @@ public class AppConfig {
         userRepository
             .findByEmail(userEmail)
             .orElseThrow(() -> new UsernameNotFoundException("User Not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
